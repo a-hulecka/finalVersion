13e2ff7011f1b23598310575ac02552c11510795
aleksandra hulecka <aleksandra.a.hulecka@gmail.com>
2019-10-28 15:27:53 +0100

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './containers/items/items.component';
+import { WorkersComponent } from './containers/workers/workers.component';
+import { RegisterComponent } from './containers/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'items', component: ItemsComponent},
+  {path: 'workers', component: WorkersComponent},
+  {path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule } from 'ngx-bootstrap';
+import { ContainersComponent } from './containers/containers.component';
+import { ItemsComponent } from './containers/items/items.component';
+import { WorkersComponent } from './containers/workers/workers.component';
+import { RegisterComponent } from './containers/register/register.component';
+import { FormComponent } from './containers/register/form/form.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './containers/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainersComponent,
+    ItemsComponent,
+    WorkersComponent,
+    RegisterComponent,
+    FormComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
